de3d99a8d5671c06656ce04656b2b6cc658b3d86
Amphei <aleksandaratanasovski1994@gmail.com>
2024-01-23 00:41:43 +0100

    improved carousel styling

--- components/ProductCarousel.vue
+++ components/ProductCarousel.vue
@@ -11,8 +11,7 @@ const products = computed(() => productStore.topProducts)
   <div
     id="carouselExampleDark"
     class="carousel carousel-dark slide mb-2"
-    data-bs-ride="carousel"
-  >
+    data-bs-ride="carousel">
     <div class="carousel-indicators">
       <button
         v-for="(product, index) in products"
@@ -20,23 +19,20 @@ const products = computed(() => productStore.topProducts)
         :data-bs-target="'#carouselExampleDark'"
         :data-bs-slide-to="index"
         :class="{ active: index === 0 }"
-        aria-label="Slide {{ index + 1 }}"
-      ></button>
+        aria-label="Slide {{ index + 1 }}"></button>
     </div>
     <div class="carousel-inner">
       <div
         v-for="(product, index) in products"
-        :key="product._id"
+        :key="product._id.toString()"
         :class="['carousel-item', { active: index === 0 }]"
-        :data-bs-interval="index === 0 ? 5000 : 3000"
-      >
+        :data-bs-interval="index === 0 ? 5000 : 3000">
         <NuxtLink :to="`/product/${product._id}`">
           <NuxtImg
             provider="cloudinary"
             :src="product.image"
             class="d-block caroimg img-fluid"
-            :alt="product.name"
-          />
+            :alt="product.name" />
           <div class="carousel-caption">
             <h2 class="text-white">{{ product.name }}</h2>
             <p class="text-white">{{ `$${product.price}` }}</p>
@@ -48,8 +44,7 @@ const products = computed(() => productStore.topProducts)
       class="carousel-control-prev"
       type="button"
       data-bs-target="#carouselExampleDark"
-      data-bs-slide="prev"
-    >
+      data-bs-slide="prev">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
       <span class="visually-hidden">Previous</span>
     </button>
@@ -57,8 +52,7 @@ const products = computed(() => productStore.topProducts)
       class="carousel-control-next"
       type="button"
       data-bs-target="#carouselExampleDark"
-      data-bs-slide="next"
-    >
+      data-bs-slide="next">
       <span class="carousel-control-next-icon" aria-hidden="true"></span>
       <span class="visually-hidden">Next</span>
     </button>
@@ -68,7 +62,9 @@ const products = computed(() => productStore.topProducts)
 <style scoped>
 .caroimg {
   width: 100%;
+  height: auto;
   max-height: 480px;
+  object-fit: contain;
 }
 
 .carousel-caption {
